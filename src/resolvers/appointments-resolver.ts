a8935fb4c2e0931f3b110dd7f0d469300a1e70e4
Alyxson Marques <alyxson.marques0@gmail.com>
2023-06-08 21:01:48 -0300

    feature: appointment creation (model, resolver, input)

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,9 +1,21 @@
-import { Query, Resolver } from "type-graphql";
+import { Query, Resolver, Mutation, Arg } from "type-graphql";
+import { CreateAppointmentInput } from "../dtos/inputs/create-appointment-input";
+import { AppointmentModel } from "../dtos/models/appointment-model";
 
 @Resolver()
 export class AppointmentsResolver {
   @Query(() => String)
   async helloWorld() {
     return "Hello, World";
+  }
+
+  @Mutation(() => AppointmentModel)
+  async createAppointment(@Arg("data") data: CreateAppointmentInput) {
+    const appointment = {
+      startsAt: data.startsAt,
+      endsAt: data.endsAt,
+    };
+
+    return appointment;
   }
 }
